f1ab7189697a2dec705ba078fd00ad59708a01a4
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-10-03 21:58:48 +0300

    Initial Commit

---
+++ ApolloGraphQLWithSwiftUI/ApolloGraphQLWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApolloGraphQLWithSwiftUIApp.swift
+//  ApolloGraphQLWithSwiftUI
+//
+//  Created by Ramill Ibragimov on 03.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct ApolloGraphQLWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
